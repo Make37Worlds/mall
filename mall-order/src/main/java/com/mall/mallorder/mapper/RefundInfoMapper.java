7bf42b9bdb307111bd0c36631f3ba0ffec92d78d
Make37Worlds <515118458@qq.com>
2023-10-28 17:13:37 +0800

    rest test

--- mall-order/src/main/java/com/mall/mallorder/mapper/RefundInfoMapper.java
+++ mall-order/src/main/java/com/mall/mallorder/mapper/RefundInfoMapper.java
@@ -2,6 +2,7 @@ package com.mall.mallorder.mapper;
 
 import com.mall.mallorder.entity.RefundInfo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author Jiayu
  * @since 2023-10-25
  */
+@Mapper
 public interface RefundInfoMapper extends BaseMapper<RefundInfo> {
 
 }
