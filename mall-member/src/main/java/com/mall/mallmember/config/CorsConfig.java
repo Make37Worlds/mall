be02893b64c8c18ef42f6949c222f324da134182
Make37Worlds <515118458@qq.com>
2023-10-30 16:13:00 +0800

    cors

---
+++ mall-member/src/main/java/com/mall/mallmember/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.mall.mallmember.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 是否支持安全证书
+        config.addAllowedOrigin("*"); // 允许任何域名使用
+        config.addAllowedHeader("*"); // 允许任何头
+        config.addAllowedMethod("*"); // 允许任何方法（post、get等）
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
+
