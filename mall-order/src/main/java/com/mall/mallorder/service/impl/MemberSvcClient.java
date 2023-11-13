37661ead2054c57898db12675cbd9ca225e12b52
Make37Worlds <515118458@qq.com>
2023-11-13 23:51:36 +0800

    order svc

--- mall-order/src/main/java/com/mall/mallorder/service/impl/MemberSvcClient.java
+++ mall-order/src/main/java/com/mall/mallorder/service/impl/MemberSvcClient.java
@@ -4,6 +4,7 @@ import com.mall.mallorder.entity.DTO.MemberInfo;
 import com.mall.mallorder.entity.DTO.OrderRequest;
 import com.mall.mallorder.entity.ResultInfo;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
