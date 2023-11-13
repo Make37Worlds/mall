f7ec4cdd43167b68e519bf4041b5d6721dab44ff
Make37Worlds <515118458@qq.com>
2023-11-13 23:24:26 +0800

    order svc

--- mall-order/src/main/java/com/mall/mallorder/MallOrderApplication.java
+++ mall-order/src/main/java/com/mall/mallorder/MallOrderApplication.java
@@ -1,11 +1,13 @@
 package com.mall.mallorder;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@MapperScan("com.mall.mallorder.mapper")
 public class MallOrderApplication {
 
     public static void main(String[] args) {
---
+++ mall-order/src/main/java/com/mall/mallorder/config/MyBatisPlusConfig.java
@@ -0,0 +1,15 @@
+package com.mall.mallorder.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.mall.mallorder.mapper")
+public class MyBatisPlusConfig {
+
+}
